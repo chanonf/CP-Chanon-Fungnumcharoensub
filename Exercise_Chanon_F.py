e85a8b411cef938c04a80d44ecda9f2fcd5fe7b7
chanonf <chanon.fungnum@gmail.com>
2020-11-23 21:55:04 +0700

    Create Exercise_Chanon_F.py

---
+++ Exercise_Chanon_F.py
@@ -0,0 +1,10 @@
+FE = 100
+GB = 100
+IS = 100
+CP = 100
+
+print("your score")
+print("Foundation English",FE)
+print("Gereral Business",GB)
+print("Introduction Computer System",IS)
+print("Computer Programing",CP)
