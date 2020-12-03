df207b7863cf19fd062faaa8469ddd4063d83610
chanonf <chanon.fungnum@gmail.com>
2020-12-03 16:31:16 +0700

    Update Execrsise5_1_Chanon_F.py

--- Execrsise5_1_Chanon_F.py
+++ Execrsise5_1_Chanon_F.py
@@ -1,2 +1,29 @@
-x = int(input("eiei"))
-print(x)
+usernameInput = input("Username :")
+passwordInput = input("Password :")
+
+if usernameInput == "Chanon" and passwordInput == "1234" :
+    print("Welcome")
+    print("-----Chanon Shop-----")
+    print("1.Sandwich 20 THB")
+    print("2.Burger 50 THB")
+    print("3.Taco 40 THB")
+    userSeleted = int(input(">>"))
+
+    if userSeleted == 1:
+        amount = int(input("How many pieces"))
+        print("Total =", amount*20)
+        print("Thanks")
+    elif userSeleted == 2:
+        amount = int(input("How many pieces"))
+        print("Total =", amount*50)
+        print("Thanks")
+    elif userSeleted == 3:
+        amount = int(input("How many pieces"))
+        print("Total =", amount*40)
+        print("Thanks")
+    else:
+        print("Please try again")
+
+
+
+
